1a10553b208b20d45469580e76c0fbf69e982aa1
HAM!DREZA <hamidreza.eidy1999@gmail.com>
2023-11-14 16:18:25 +0330

    get the validation process from server

--- plugins/auth.js
+++ plugins/auth.js
@@ -1,15 +1,17 @@
-// export default defineNuxtPlugin(async (nuxtApp) => {
-//     const { authUser } = useAuth();
+export default defineNuxtPlugin(async (nuxtApp) => {
+    const { authUser } = useAuth();
+    const { public: { apiBase } } = useRuntimeConfig();
 
-//     const headers = useRequestHeaders(['cookie']);
+    const headers = useRequestHeaders(['cookie'])
 
-//     try {
-//          const user = await $fetch ('/api/auth/me', {
-//             headers
-//          })
+    try {
+        const data = await $fetch(`${apiBase}/api/me`, {
+            credentials: 'include',
+            headers: { ...headers, 'Accept': 'application/json', 'Referer': 'http://localhost:3000' }
+        })
 
-//          authUser.value = user;
-//     } catch (error) {
-//         authUser.value = null;
-//     }
-// })
+        authUser.value = data.user;
+    } catch (error) {
+        authUser.value = null;
+    }
+})
